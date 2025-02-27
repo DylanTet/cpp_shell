0d30208e9ad351e3b65c911f5f067ad062899472
Dylan <tettemerd@gmail.com>
2025-02-26 18:11:34 -0800

    REPL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main() {
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
   while (true) {
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
     std::cout << "$ ";
 
     std::string input;
