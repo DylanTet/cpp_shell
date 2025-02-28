1141cab0d493ba12d695ee1c5baa48f6bff1eb45
Dylan <tettemerd@gmail.com>
2025-02-27 16:55:44 -0800

    echo

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <functional>
 #include <iostream>
 #include <string>
+#include <string_view>
 #include <unordered_map>
+
+void check_command(std::string &command) {
+  if (command.find("echo") != std::string::npos) {
+    std::string echo_string = command.erase(0, 5);
+    std::cout << echo_string << '\n';
+  } else {
+    std::cout << command << ":"
+              << " command not found\n";
+  }
+}
 
 int main() {
   std::cout << std::unitbuf;
@@ -16,7 +27,6 @@ int main() {
       return 0;
     }
 
-    std::cout << input << ":"
-              << " command not found\n";
+    check_command(input);
   }
 }
